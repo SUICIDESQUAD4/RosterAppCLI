c079ff310d7e45b03ae886177fdcae0b037fa230
Alexangelo Andews <alexangeloandrews@gmail.com>
2025-11-26 21:30:53 -0400

    refactoring the url prefixes for existing routes v2

--- App/views/auth.py
+++ App/views/auth.py
@@ -10,45 +10,10 @@ from App.controllers import (
 
 )
 
-auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
+auth_views = Blueprint('auth_views', __name__, url_prefix="/api/auth")
 
-
-
-
-'''
-Page/Action Routes
-'''    
-
-@auth_views.route('/identify', methods=['GET'])
-@jwt_required()
-def identify_page():
-    return render_template('message.html', title="Identify", message=f"You are logged in as {current_user.id} - {current_user.username}")
-    
 
 @auth_views.route('/login', methods=['POST'])
-def login_action():
-    data = request.form
-    token = login(data['username'], data['password'])
-    response = redirect(request.referrer)
-    if not token:
-        flash('Bad username or password given'), 401
-    else:
-        flash('Login Successful')
-        set_access_cookies(response, token) 
-    return response
-
-@auth_views.route('/logout', methods=['GET'])
-def logout_action():
-    response = redirect(request.referrer) 
-    flash("Logged Out!")
-    unset_jwt_cookies(response)
-    return response
-
-'''
-API Routes
-'''
-
-@auth_views.route('/api/login', methods=['POST'])
 def user_login_api():
   data = request.json
   token = login(data['username'], data['password'])
@@ -58,12 +23,12 @@ def user_login_api():
   set_access_cookies(response, token)
   return response
 
-@auth_views.route('/api/identify', methods=['GET'])
+@auth_views.route('/identify', methods=['GET'])
 @jwt_required()
 def identify_user():
     return jsonify({'message': f"username: {current_user.username}, id : {current_user.id}"})
 
-@auth_views.route('/api/logout', methods=['GET'])
+@auth_views.route('/logout', methods=['GET'])
 def logout_api():
     response = jsonify(message="Logged Out!")
     unset_jwt_cookies(response)
